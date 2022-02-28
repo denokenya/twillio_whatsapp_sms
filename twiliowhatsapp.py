60cafb3f31eecbc070bc2b67d87e80ee56a0a9cf
denokenya <dennismuteti25@gmail.com>
2022-02-28 16:11:48 +0300

    initial commit

---
+++ twiliowhatsapp.py
@@ -0,0 +1,21 @@
+from twilio.rest import Client 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+
+
+first_name = input("What is your first name ?\n")
+message = client.messages.create( 
+                              from_=os.getenv('TWILIO_WHATSAPP_FROM'),  
+                              body=f"Hello {first_name.capitalize()} how are you doing today ?",      
+                              to=os.getenv('TWILIO_WHATSAPP_TO') 
+                          ) 
+ 
+print(message.sid)
+print(f" Hello ,{first_name.capitalize()} we have sent you a whatsapp message!")
---
+++ twilliosms.py
@@ -0,0 +1,19 @@
+from twilio.rest import Client 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+ 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create(  
+                              messaging_service_sid=os.getenv('TWILIO_MESSAGING_SERVICE_SID') , 
+                              body='Hello How are you Sir?',      
+                              to=os.getenv('TWILIO_TO'), 
+                          ) 
+ 
+print(message.sid)
